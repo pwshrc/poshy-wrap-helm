0e3746d05ab1091be07452c96ed44d7108800e70
Bruce Markham <219281+brucificus@users.noreply.github.com>
2024-12-18 13:34:41 -0500

    🏗️ Splits PSM1, Removes `#Requires` from PSM1
    Tests were failing because the module was not importable. The `#Requires` syntax being used in the root PSM1 prevented dependencies from being fully resolved before the module loading could complete, making the module unusable.

--- src/poshy-wrap-helm.psm1
+++ src/poshy-wrap-helm.psm1
@@ -1,32 +1,10 @@
 #!/usr/bin/env pwsh
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
-#Requires -Modules @{ ModuleName = "poshy-lucidity"; RequiredVersion = "0.4.1" }
 
 
-if (-not (Test-Command helm) -and (-not (Get-Variable -Name PWSHRC_FORCE_MODULES_EXPORT_UNSUPPORTED -Scope Global -ValueOnly -ErrorAction SilentlyContinue))) {
+if (-not (Get-Command helm -ErrorAction SilentlyContinue) -and (-not (Get-Variable -Name PWSHRC_FORCE_MODULES_EXPORT_UNSUPPORTED -Scope Global -ValueOnly -ErrorAction SilentlyContinue))) {
     return
+} else {
+    . "$PSScriptRoot/poshy-wrap-helm.ps1"
 }
-
-function Invoke-Helm {
-    helm @args
-}
-Set-Alias -Name h -Value Invoke-Helm
-
-function Invoke-HelmInstall {
-    helm install @args
-}
-Set-Alias -Name hin -Value Invoke-HelmInstall
-
-function Invoke-HelmSearch {
-    helm search @args
-}
-Set-Alias -Name hse -Value Invoke-HelmSearch
-
-function Invoke-HelmUpgrade {
-    helm upgrade @args
-}
-Set-Alias -Name hup -Value Invoke-HelmUpgrade
-
-
-Export-ModuleMember -Function * -Alias *
